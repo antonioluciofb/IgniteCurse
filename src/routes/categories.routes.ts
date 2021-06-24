46d0867c09673e7f5b90fcd59483facfbd8dd772
antonioluciofb <antonioluciofb@hotmail.com>
2021-06-24 18:07:38 -0300

    .

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,10 @@
-import { request, response, Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/', (request, response) => {
+categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
   categoriesRepository.create({ name, description });
